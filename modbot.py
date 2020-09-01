f9154ab3f75b3eac66ab8662aea8b9b0de84e1ea
Ananta Srikar <srikarananta01@gmail.com>
2020-09-01 08:46:10 +0530

    Converted indentations to tabs

--- modbot.py
+++ modbot.py
@@ -6,17 +6,17 @@ bot = commands.Bot(command_prefix='!')
 bot.remove_command('help') 
 
 def getToken():
-    tokenFile = open('TOKEN.txt','r')
-    tokentxt = tokenFile.read()
-    return tokentxt
+	tokenFile = open('TOKEN.txt','r')
+	tokentxt = tokenFile.read()
+	return tokentxt
 
 @bot.event
 async def on_ready():
-    print('logged in as '+str(bot.user))
+	print('logged in as '+str(bot.user))
 
 @bot.command(name = "help")
 async def help(ctx):
-    await ctx.channel.send('''
+	await ctx.channel.send('''
 Command prefix: !
 Commands:
 join-batch1: to get the role of "Batch 1"
@@ -24,18 +24,18 @@ join-batch2: to get the role of "Batch 2"''')
 
 @bot.command(name = "join-batch1")
 async def batch1(ctx):
-    server = ctx.guild
-    await server.create_role(name="Bitchboy",colour=discord.Colour(0x00FFFF))
-    user = ctx.message.author
-    role = discord.utils.get(ctx.guild.roles, name = "Batch 1")
-    await user.add_roles(role)
+	server = ctx.guild
+	await server.create_role(name="Bitchboy",colour=discord.Colour(0x00FFFF))
+	user = ctx.message.author
+	role = discord.utils.get(ctx.guild.roles, name = "Batch 1")
+	await user.add_roles(role)
 
 @bot.command(name = "join-batch2")
 async def batch2(ctx):
-    server = ctx.guild
-    await server.create_role(name="Bitchboy",colour=discord.Colour(0x00FFFF))
-    user = ctx.message.author
-    role = discord.utils.get(ctx.guild.roles, name = "Batch 2")
-    await user.add_roles(role)
+	server = ctx.guild
+	await server.create_role(name="Bitchboy",colour=discord.Colour(0x00FFFF))
+	user = ctx.message.author
+	role = discord.utils.get(ctx.guild.roles, name = "Batch 2")
+	await user.add_roles(role)
 
 bot.run (getToken())
