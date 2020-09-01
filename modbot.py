c9ad0a6df00aea2181de1f1635cf59ca07b44d6f
Ananta Srikar <srikarananta01@gmail.com>
2020-09-01 10:26:40 +0530

    Made batch specific commands and improved error handling

--- modbot.py
+++ modbot.py
@@ -1,8 +1,8 @@
 import discord
-from discord.ext import commands
+from discord.ext.commands import Bot, CommandNotFound
 from discord.utils import get
 
-bot = commands.Bot(command_prefix='!')
+bot = Bot(command_prefix='!')
 bot.remove_command('help') 
 
 def getToken():
@@ -18,24 +18,68 @@ async def on_ready():
 async def help(ctx):
 	await ctx.channel.send('''
 Command prefix: !
-Commands:
-join-batch1: to get the role of "Batch 1"
-join-batch2: to get the role of "Batch 2"''')
 
-@bot.command(name = "join-batch1")
-async def batch1(ctx):
-	server = ctx.guild
-	await server.create_role(name="Bitchboy",colour=discord.Colour(0x00FFFF))
+Command: 
+
+join-batch `batchname`
+
+Eg: join-batch `batch-1`
+
+leave-batch `batchname`
+
+''')
+
+@bot.command(name = 'join-batch')
+async def join_batch(ctx, *args):
+
 	user = ctx.message.author
-	role = discord.utils.get(ctx.guild.roles, name = "Batch 1")
-	await user.add_roles(role)
 
-@bot.command(name = "join-batch2")
-async def batch2(ctx):
-	server = ctx.guild
-	await server.create_role(name="Bitchboy",colour=discord.Colour(0x00FFFF))
+	if not args:
+		await ctx.channel.send("Try entering a batch for a role")
+	
+	elif len(args) != 1:
+		await ctx.channel.send("Please enter only one batch")
+	
+	elif args[0] != "batch-1" and args[0] != "batch-2":
+		await ctx.channel.send("Enter a valid batch")
+
+	elif 'batch-1' in [y.name for y in user.roles] or 'batch-2' in [y.name for y in user.roles]:
+		await ctx.channel.send("Sorry, can't have 2 batches at the same time. Leave batch and assign again")
+	
+	else:
+		
+		req_role = args[0]
+
+		try:
+			role = discord.utils.get(ctx.guild.roles, name = req_role)
+			await user.add_roles(role)
+			await ctx.channel.send("Added Role {} to {}".format(role.name, user.mention))
+		
+		except :
+			await ctx.channel.send("Looks like the role {} isn't on this server.".format(req_role))
+
+
+@bot.command(name = 'leave-batch')
+async def leave_batch(ctx):
+
 	user = ctx.message.author
-	role = discord.utils.get(ctx.guild.roles, name = "Batch 2")
-	await user.add_roles(role)
 
-bot.run (getToken())
+	remove_role = None
+
+	for role in user.roles:
+		if 'batch' in role.name:
+			remove_role = role
+			break
+
+	if remove_role:
+		await user.remove_roles(remove_role)
+		await ctx.channel.send("Removed {} from {}".format(user.mention, remove_role))
+	
+	else:
+		await ctx.channel.send("Can't remove you since you haven't joined any batch")
+
+
+
+bot.run(getToken())
+
+#user.remove_role(role)
